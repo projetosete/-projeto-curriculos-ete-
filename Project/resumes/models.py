64724c117e452a38db19a70fb7c990d7c79a08da
douglasbispo <dsanttos363@gmail.com>
2022-03-30 02:38:17 -0300

    Adcionado: Função para dar nome as imagens adicionadas. Modificação em 'settings.py, urls.py\project, models.py'

--- Project/resumes/models.py
+++ Project/resumes/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
+def upload_image_resume(instance, filename):
+    return f"{instance.name}-{str(uuid.uuid4())}-{filename}"
+    
 
 class Resume(models.Model):
     name = models.CharField(max_length=60, blank=False, null=False)
     date = models.DateField(blank=False, null=False)
     phone = models.CharField(max_length=15, blank=False, null=False)
     city = models.CharField(max_length=30, blank=False, null=False)
-    image = models.ImageField(default='default.png', upload_to='profile_images')
+    image = models.ImageField(upload_to=upload_image_resume, blank=False, null=False)
     course = models.CharField(max_length=50, blank=False, null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
